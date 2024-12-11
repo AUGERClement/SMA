59d9796c79d58ce7b8001e14fe14f61fe8b89ae7
AUGERClement <clement.auger@epitech.eu>
2024-12-11 13:44:56 +0100

    Step 2

--- lib/sma.ex
+++ lib/sma.ex
@@ -39,11 +39,11 @@ defmodule Sma do
     - 24hr low
     - 24hr volume
 
-  argument:
+  argument :
     - pair
     the concerned pair, in a binance compatible format (default value is "BTCUSDT")
 
-  return:
+  return :
     Nothing
   """
   def fetch_24hr_data(pair \\ "BTCUSDT") do
@@ -55,29 +55,48 @@ defmodule Sma do
   end
 
   @doc """
-  Function fetching and displaying data on the binance public API for a defined pair (step 1 of the case study in README)
-  The data we are looking for are :
-    - Current price
-    - 24hr high
-    - 24hr low
-    - 24hr volume
+  Function fetching the last prices of a pair.
 
-  argument:
+  arguments :
     - pair
     The concerned pair, in a binance compatible format (default value is "BTCUSDT")
     - limit
     The maximum number of trades to consult
 
-  return:
+  return :
     A list of prices, of size 'limit'
   """
   def fetch_last_prices(pair \\ "BTCUSDT", limit \\ 30) do
     "https://api.binance.com/api/v3/trades"
     |> HTTPoison.get!([], params: %{symbol: pair, limit: limit}) #empty header
-    |> IO.inspect()
+    |> extract_body()
+    #|> IO.inspect()
+    |> Enum.map(fn x -> x["price"] end) # Extract price data from each trade
+    |> Enum.map(fn x -> String.to_float(x) end) # Convert to number
+  end
+
+  @doc """
+  Function computing the Simple Moving Average of a asset.
+  More infos can be found on https://www.investopedia.com/terms/s/sma.asp
+
+  argument :
+    - prices
+    An array of prices
+
+  return :
+    The computed SMA
+  """
+  def computeSMA(prices) do
+    Enum.sum(prices) / Enum.count(prices)
   end
 end
 
 Application.ensure_all_started(:httpoison) # Ensure HTTPoison is running
+
+# Step 1
 Sma.fetch_24hr_data()
-#Sma.fetch_last_prices()
+
+# Step 2
+Sma.fetch_last_prices()
+|> Sma.computeSMA()
+|> IO.inspect(label: "The SMA is ")
